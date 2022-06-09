5e16dcb409f7a993d7edee404e78ee5d6ed14c12
Rungtiwa Chanaka <rungtiwa.c24@gmail.com>
2022-06-09 22:26:02 +1000

    update

--- index.js
+++ index.js
@@ -42,7 +42,7 @@ mongoose
     
     app.get('*', (req, res) => {
         res.sendFile(path.join(__dirname, '/client/build', 'index.html'));
-    }),
+    });
     
 
 app.listen(process.env.PORT || 5000, () => {
