15aee050002e7973c7c06c784cffccf5a5044729
Rungtiwa Chanaka <rungtiwa.c24@gmail.com>
2022-06-10 15:45:58 +1000

    update

--- index.js
+++ index.js
@@ -38,10 +38,10 @@ mongoose
     app.use('/api/posts', postRoute);
     app.use('/api/categories', categoryRoute);
 
-    app.use(express.static(path.join(__dirname, "/client/build")));
+    app.use(express.static(path.join(__dirname, "client/build")));
     
     app.get('*', (req, res) => {
-        res.sendFile(path.join(__dirname, '/client/public', 'index.html'));
+        res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
     });
     
 
